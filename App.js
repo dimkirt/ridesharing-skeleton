6825afd74181345a4d9057099714bf6d8b7674e6
Dimitrios Kirtsios <dimkirts@gmail.com>
2018-02-14 17:22:55 +0200

    Get User location, ask for permission

--- App.js
+++ App.js
@@ -1,58 +1,79 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
   Text,
-  View
+  View,
+  PermissionsAndroid
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class GeoLocationExample extends Component{
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
+  componentDidMount(){
+    this._requestPermission();
+  }
+
+  /*
+    Android API >= 23 Requires an additional step to check for, and request the
+    ACCESS_FINE_LOCATION permission using the PermissionsAndroid API. Failure to
+    do so may result in a hard crash.
+    targetSdkVersion and compiledSdkVersion must be over >= 23 in order for the
+    dialog prompt to come up
+  */
+  _requestPermission(){
+    return PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, 
+      {
+        'title': 'Smartwheels App Location Permission',
+        'message': 'Smartwheels App needs access to your GPS'
+      })
+      .then(result => {
+        if (result === PermissionsAndroid.RESULTS.GRANTED){
+          // Location Permission granted
+          navigator.geolocation.getCurrentPosition(
+            position => {
+              // success cb
+              this.setState({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                error: null,
+              });
+            },
+            error => {
+              // error cb
+              this.setState({error: error.message});
+            },
+            {
+              // settings object
+              // Others except timeout seem to create a bug
+              //enableHighAccuracy: true,
+              timeout: 20000,
+              //maximumAge: 1000
+            }
+          );
+        }
+        else{
+          // Location Permission NOT granted
+          this.setState({error: 'Permission Denied!'});
+        }
+    })
+  }
+
+  render(){
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Latitude: {this.state.latitude}</Text>
+        <Text>Longitude: {this.state.longitude}</Text>
+        {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
