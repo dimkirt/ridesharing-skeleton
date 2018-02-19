bedc5c5b4404c7e9d4a483aac82b8302156acd7f
Dimitrios Kirtsios <dimkirts@gmail.com>
2018-02-19 18:34:15 +0200

    Setup maps, add lines and pins, custom pins

--- App.js
+++ App.js
@@ -3,11 +3,42 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  PermissionsAndroid
+  StyleSheet,
+  PermissionsAndroid,
+  Dimensions
 } from 'react-native';
+
+import pinImage from './assets/custompin.png';
 
 // import firebase
 import firebase from 'react-native-firebase';
+
+// import react-native maps
+import MapView, {Marker, Polyline} from 'react-native-maps';
+
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
 
 export default class GeoLocationExample extends Component{
 
@@ -16,11 +47,95 @@ export default class GeoLocationExample extends Component{
 
     // Add firebase things to state too
     this.state = {
+      region: {
+        latitude: 48.1769318,
+        longitude: 11.5940593,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
       latitude: null,
       longitude: null,
       error: null,
-      notif: null
+      notif: null,
+      route2: [
+        {
+          key: 11,
+          title: "Point A",
+          coordinates:{
+            latitude: 48.175133,
+            longitude:  11.593923
+          },
+          description: "Start of the Test Route 2"
+        },
+        {
+          key: 22,
+          title: "Point B",
+          coordinates:{
+            latitude: 48.174460,
+            longitude: 11.595318
+          },
+          description: "Mid of the Test Route 2"
+        },
+        {
+          key: 33,
+          title: "Point C",
+          coordinates:{
+            latitude: 48.172986,
+            longitude: 11.598118
+          },
+          description: "End of the Test Route"
+        }
+      ],
+      route1: [
+        {
+          key: 1,
+          title: "Alte Heide",
+          coordinates:{
+            latitude: 48.177730,
+            longitude: 11.602100
+          },
+          description: "Start of the Test Route"
+        },
+        {
+          key: 2,
+          title: "Nordfriedhof",
+          coordinates:{
+            latitude: 48.173058,
+            longitude: 11.596876
+          },
+          description: "2nd point of the Test Route"
+        },
+        {
+          key: 3,
+          title: "Mid",
+          coordinates:{
+            latitude: 48.170099,
+            longitude: 11.594032
+          },
+          description: "3rd point of the Test Route"
+        },
+        {
+          key: 4,
+          title: "Potsdamer Strasse",
+          coordinates:{
+            latitude: 48.1666106,
+            longitude: 11.5903105
+          },
+          description: "4th point of the Test Route"
+        },
+        {
+          key: 5,
+          title: "Germania Munich",
+          coordinates:{
+            latitude: 48.167256,
+            longitude: 11.5883395  
+          },
+          description: "End of the Test Route"
+        }
+      ]      
     };
+
+    this.onRegionChange = this.onRegionChange.bind(this);
   }
 
   _setupFirebase(){
@@ -106,14 +221,64 @@ export default class GeoLocationExample extends Component{
     })
   }
 
+  onRegionChange(event){
+    //this.setState({region: event});
+  }
+
   render(){
+    const markerElems = this.state.route1.map(marker => (
+      <Marker
+        key={marker.key}
+        coordinate={marker.coordinates}
+        title={marker.title}
+        description={marker.description}
+      />
+    ));
+
+    const markerElems2 = this.state.route2.map(marker => (
+      <Marker 
+        key={marker.key}
+        coordinate={marker.coordinates}
+        title={marker.title}
+        description={marker.description}
+        image={pinImage}
+      />
+    ));
+
+    const routeLine1 = this.state.route1.map(marker => marker.coordinates);
+    const routeLine2 = this.state.route2.map(marker => marker.coordinates);
+
     return (
-      <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.container}>
+        <MapView style={styles.map} 
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+        >
+          {markerElems}
+          {markerElems2}
+
+          <Polyline
+            key={1}
+            coordinates={routeLine1}
+            strokeColor="#F00"
+            fillColor="rgba(255,0,0,0.5)"
+            strokeWidth={1}
+          />
+
+          <Polyline
+            key={2}
+            coordinates={routeLine2}
+            strokeColor="#0F0"
+            fillColor="rgba(255,0,0,0.5)"
+            strokeWidth={3}
+          />
+        </MapView>
         <Text>Latitude: {this.state.latitude}</Text>
         <Text>Longitude: {this.state.longitude}</Text>
         <Text>Notif: {this.state.notif}</Text>
         {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
       </View>
+      
     );
   }
 }
